c103577075fef9bd6dc18ab77f600e014d6964b6
SleepTee <kaka4uka@mail.ru>
2021-01-19 20:16:03 +0300

    add rfpassport

---
+++ rfpassport.js
@@ -0,0 +1,87 @@
+let recognized_data = [{"language":"ru","location":{"height":30,"width":32,"x":321,"y":1710},"text":"о"},{"language":"ru","location":{"height":27,"width":32,"x":321,"y":1656},"text":"с"},{"language":"ru","location":{"height":28,"width":32,"x":321,"y":1608},"text":"с"},{"language":"ru","location":{"height":32,"width":32,"x":321,"y":1549},"text":"и"},{"language":"ru","location":{"height":37,"width":32,"x":321,"y":1432},"text":"с"},{"language":"ru","location":{"height":32,"width":32,"x":321,"y":1373},"text":"к"},{"language":"ru","location":{"height":28,"width":32,"x":320,"y":1318},"text":"а"},{"language":"ru","location":{"height":22,"width":32,"x":320,"y":1276},"text":"я"},{"language":"ru","location":{"height":27,"width":29,"x":322,"y":1006},"text":"Е"},{"language":"ru","location":{"height":28,"width":28,"x":322,"y":953},"text":"Р"},{"language":"ru","location":{"height":28,"width":28,"x":322,"y":900},"text":"А"},{"language":"ru","location":{"height":28,"width":28,"x":322,"y":847},"text":"Ц"},{"language":"ru","location":{"height":27,"width":28,"x":322,"y":747},"text":"я"},{"language":"ru","location":{"height":96,"width":20,"x":450,"y":1793},"text":"Паспорт"},{"language":"ru","location":{"height":66,"width":20,"x":450,"y":1716},"text":"выдан"},{"language":"ru","location":{"height":52,"width":41,"x":424,"y":1468},"text":"ТП"},{"language":"ru","location":{"height":73,"width":41,"x":423,"y":1369},"text":"№1"},{"language":"ru","location":{"height":135,"width":41,"x":422,"y":1167},"text":"ГОР."},{"language":"ru","location":{"height":272,"width":42,"x":420,"y":868},"text":"ПОДОЛЬСКЕ"},{"language":"ru","location":{"height":146,"width":40,"x":496,"y":1493},"text":"ОУФМС"},{"language":"ru","location":{"height":179,"width":41,"x":495,"y":1280},"text":"РОССИИ"},{"language":"ru","location":{"height":66,"width":40,"x":495,"y":1189},"text":"ПО"},{"language":"ru","location":{"height":301,"width":41,"x":494,"y":863},"text":"МОСКОВСКОЙ"},{"language":"ru","location":{"height":118,"width":40,"x":493,"y":720},"text":"ОБЛ."},{"language":"ru","location":{"height":119,"width":39,"x":571,"y":1253},"text":"гор."},{"language":"ru","location":{"height":272,"width":41,"x":569,"y":953},"text":"подольске"},{"language":"ru","location":{"height":205,"width":44,"x":660,"y":930},"text":"500-120"},{"language":"ru","location":{"height":55,"width":35,"x":687,"y":1834},"text":"Дата"},{"language":"ru","location":{"height":83,"width":34,"x":678,"y":1742},"text":"выдачи"},{"language":"ru","location":{"height":74,"width":29,"x":674,"y":1653},"text":"23."},{"language":"ru","location":{"height":72,"width":26,"x":674,"y":1566},"text":"04."},{"language":"ru","location":{"height":106,"width":29,"x":675,"y":1440},"text":"2008"},{"language":"ru","location":{"height":41,"width":27,"x":679,"y":1376},"text":"Код"},{"language":"ru","location":{"height":169,"width":31,"x":681,"y":1196},"text":"подразделения"},{"language":"ru","location":{"height":84,"width":20,"x":869,"y":912},"text":"Личный"},{"language":"ru","location":{"height":39,"width":19,"x":870,"y":864},"text":"код"},{"language":"ru","location":{"height":84,"width":20,"x":1026,"y":938},"text":"Анчная"},{"language":"ru","location":{"height":93,"width":20,"x":1026,"y":836},"text":"подпить"},{"language":"ru","location":{"height":108,"width":25,"x":1388,"y":1893},"text":"россия"},{"language":"ru","location":{"height":108,"width":26,"x":1386,"y":1451},"text":"россия"},{"language":"ru","location":{"height":110,"width":26,"x":1384,"y":1004},"text":"россия"},{"language":"ru","location":{"height":89,"width":18,"x":1467,"y":1404},"text":"Фамилия"},{"language":"ru","location":{"height":237,"width":39,"x":1433,"y":1017},"text":"богданов"},{"language":"ru","location":{"height":96,"width":20,"x":1680,"y":1398},"text":"Отчество"},{"language":"ru","location":{"height":180,"width":43,"x":1582,"y":1049},"text":"сергей"},{"language":"ru","location":{"height":89,"width":36,"x":1604,"y":1017},"text":"россии"},{"language":"ru","location":{"height":289,"width":44,"x":1651,"y":1003},"text":"АЛЕКСЕЕВИЧ"},{"language":"ru","location":{"height":108,"width":24,"x":1614,"y":554},"text":"РОССИЯ"},{"language":"ru","location":{"height":40,"width":30,"x":1695,"y":594},"text":"00"},{"language":"ru","location":{"height":51,"width":21,"x":1725,"y":1219},"text":"Дата"},{"language":"ru","location":{"height":106,"width":20,"x":1753,"y":1165},"text":"рождения"},{"language":"ru","location":{"height":300,"width":42,"x":1734,"y":811},"text":"25.03.1963"},{"language":"ru","location":{"height":62,"width":18,"x":1798,"y":1429},"text":"Места"},{"language":"ru","location":{"height":104,"width":18,"x":1825,"y":1389},"text":"рождения"},{"language":"ru","location":{"height":122,"width":39,"x":1793,"y":1232},"text":"ГОР."},{"language":"ru","location":{"height":235,"width":40,"x":1794,"y":973},"text":"ПОДОЛЬСК"},{"language":"ru","location":{"height":108,"width":25,"x":1834,"y":1002},"text":"россия"},{"language":"ru","location":{"height":302,"width":45,"x":1865,"y":1051},"text":"московской"},{"language":"ru","location":{"height":119,"width":42,"x":1871,"y":903},"text":"обл."},{"language":"en","location":{"height":32,"width":29,"x":1920,"y":598},"text":"co"},{"language":"ru","location":{"height":110,"width":25,"x":2066,"y":1887},"text":"россия"},{"language":"ru","location":{"height":107,"width":26,"x":2064,"y":1444},"text":"россия"},{"language":"ru","location":{"height":108,"width":25,"x":2062,"y":1002},"text":"россия"},{"language":"ru","location":{"height":108,"width":24,"x":2063,"y":554},"text":"россия"}]
+let google_sheet_data = [["ОМВД  г. Душанбе","DIA in Dushanbe"],["ОМВД Хатлонской области","DIA in Khatlon"],["ОМВД г. Гиссар","DIA in Hisor"],["ОМВД г. Истаравшан","DIA in Istaravshan"],["ОМВД г. Канибадам","DIA in Konibodom "],["ОМВД г. Пенджикент","DIA in Panjakent"],["ОМВД Дарвазского района","DIA in Darvoz "],["ОМВД района Деваштич","DIA in Devashtich"],["ОМВД г. Исфара","DIA in Isfara"],["ОМВД Кубодиёнского района","DIA in Qubodiyon"],["ОМВД Нурабадского района","DIA in Nurobod"],["ОМВД Раштского района","DIA in Rasht"],["ОМВД Самгар","DIA in Samgar"],["ОМВД Таджикабадского района","DIA in Tojikobod "],["ОМВД Турсунзаде","DIA Tursunzoda "],["ОМВД-1 г. Исфара","DIA-1 in Isfara"],["ОМВД  Яванcкого района","DIA in Yovon"],["ОМВД Вахшского района","DIA in Vakhsh"],["ОМВД района Дусти","DIA in Dusti"]];
+let personal_data = {
+    "passport": {},
+    "patent": {},
+    "migration_card": {}
+};
+let is_passport = false,
+    is_migration_card = false,
+    is_patent = false,
+    supposed_values = [];
+let other_docs_keywords = [
+    /дмс|страхово[йе]|полис/i,
+    /чек|платеж|плательщик/i,
+    /перевод/i,
+    /опись|расписка|уведомлен/i,
+    /visas|владелец|гражданина/i,
+    /holder/,   // это слово должно быть всегда чувствительным к регистру
+    /анкет[аы]|соискателя/i,
+    /работник/i,
+];
+
+let includes_passport_keywords = false,
+    includes_patent_keywords = false,
+    includes_migration_card_keywords = false,
+    recognized_data_length = recognized_data.length;
+
+function hasOtherKeywords(recognized_data, reg_exp_list) {
+    for (let item of recognized_data) {
+        for (let reg_exp of reg_exp_list) {
+            if (reg_exp.test(item['text'])) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+for (let item of recognized_data) {
+    // отсутствие сравнения кол-ва ключей паспорта
+    // с 0 позволяет обнаруживать дополнительные
+    // титульные страницы паспортов, если паспорт
+    // в документе не один
+    if (/passport|паспорт|identity|личности/i.test(item['text'])) {
+        includes_passport_keywords = true;
+    }
+
+    // 12 - общее количество распознаваемых полей паспорта
+    if (Object.keys(personal_data['passport']).length > 0
+        && Object.keys(personal_data['passport']).length < 12
+        && /орган|authority|кем/i.test(item['text'])) {
+        includes_passport_keywords = true;
+    }
+
+    if (Object.keys(personal_data['migration_card']).length === 0
+        && /departure|migrat/i.test(item['text'])) {
+        includes_migration_card_keywords = true;
+    }
+
+    if (Object.keys(personal_data['patent']).length === 0
+        && /патент/i.test(item['text'])) {
+        includes_patent_keywords = true;
+    }
+
+    // 13 - общее количество распознаваемых  полей патента
+    if (Object.keys(personal_data['patent']).length > 0
+        && Object.keys(personal_data['patent']).length < 13
+        && /кем/i.test(item['text'])) {
+        includes_patent_keywords = true;
+    }
+
+    if (
+        includes_passport_keywords
+        || includes_patent_keywords
+        || includes_migration_card_keywords
+    ) {
+        break;
+    }
+}
+
+let is_other_document = hasOtherKeywords(recognized_data, other_docs_keywords);
+
+is_passport = includes_passport_keywords ? !is_other_document : is_passport;
+is_patent = includes_patent_keywords ? !is_other_document : is_patent;
+is_migration_card = includes_migration_card_keywords ? !is_other_document : is_migration_card;
+
